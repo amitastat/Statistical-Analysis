b480e0bd528c6446028378ca40ecca75930f0d91
amitastat <115801698+amitastat@users.noreply.github.com>
2023-06-17 14:44:15 +0530

    Add files via upload

---
+++ scatter.R
@@ -0,0 +1,16 @@
+# Get the input values.
+X=3:10
+Y=c(2.3,2.7,3.1,3.6,3.8,4,4.3,4.5)
+
+# Plot the chart with 
+#age and height.
+plot(X,Y)
+
+#use the parameter main in the syntax
+plot(X,Y,main="Age versus Height")
+
+
+#use the parameter xlab and ylab in the syntax to describe the X-axis and Y-axis respectively
+plot(X,Y,main="Age versus Height",xlab="Age",ylab="Height")
+
+
